7f8e48987e855019b4466f11cb2996e90ad8d5da
ebpicgamer-spec <ebpicgamer@gmail.com>
2026-09-20 11:37:12 -0700

    Expand calculator coverage

--- src/test/java/com/blowpipecalculator/BlowpipeCalculatorTest.java
+++ src/test/java/com/blowpipecalculator/BlowpipeCalculatorTest.java
@@ -7,21 +7,44 @@ import static org.junit.Assert.assertEquals;
 public class BlowpipeCalculatorTest
 {
     @Test
-    public void testAssemblerDartsToScales()
-    {
-        assertEquals(3334, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.ASSEMBLER));
-    }
-
-    @Test
-    public void testAssemblerScalesToDarts()
-    {
-        assertEquals(999, BlowpipeCalculator.dartsForScales(3333, AmmoSaveRate.ASSEMBLER));
-        assertEquals(1000, BlowpipeCalculator.dartsForScales(3334, AmmoSaveRate.ASSEMBLER));
-    }
-
-    @Test
-    public void testNoAva()
+    public void testDartsToScalesForAllCapeSlots()
     {
         assertEquals(667, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.NONE));
+        assertEquals(1667, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.ATTRACTOR));
+        assertEquals(2381, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.ACCUMULATOR));
+        assertEquals(3334, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.ASSEMBLER));
+        assertEquals(3334, BlowpipeCalculator.scalesForDarts(1000, AmmoSaveRate.QUIVER));
+    }
+
+    @Test
+    public void testScalesToDartsForAllCapeSlots()
+    {
+        assertEquals(1500, BlowpipeCalculator.dartsForScales(1000, AmmoSaveRate.NONE));
+        assertEquals(600, BlowpipeCalculator.dartsForScales(1000, AmmoSaveRate.ATTRACTOR));
+        assertEquals(420, BlowpipeCalculator.dartsForScales(1000, AmmoSaveRate.ACCUMULATOR));
+        assertEquals(300, BlowpipeCalculator.dartsForScales(1000, AmmoSaveRate.ASSEMBLER));
+        assertEquals(300, BlowpipeCalculator.dartsForScales(1000, AmmoSaveRate.QUIVER));
+    }
+
+    @Test
+    public void testZeroInputs()
+    {
+        for (AmmoSaveRate rate : AmmoSaveRate.values())
+        {
+            assertEquals(0, BlowpipeCalculator.scalesForDarts(0, rate));
+            assertEquals(0, BlowpipeCalculator.dartsForScales(0, rate));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeDartsRejected()
+    {
+        BlowpipeCalculator.scalesForDarts(-1, AmmoSaveRate.ASSEMBLER);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeScalesRejected()
+    {
+        BlowpipeCalculator.dartsForScales(-1, AmmoSaveRate.ASSEMBLER);
     }
 }
