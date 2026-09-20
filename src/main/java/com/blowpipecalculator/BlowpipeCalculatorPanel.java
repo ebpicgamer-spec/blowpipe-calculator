c7786d1975e07acce69cba4389f5fcbc94202001
ebpicgamer-spec <ebpicgamer@gmail.com>
2026-09-20 11:51:09 -0700

    Remove redundant estimate note from panel

--- src/main/java/com/blowpipecalculator/BlowpipeCalculatorPanel.java
+++ src/main/java/com/blowpipecalculator/BlowpipeCalculatorPanel.java
@@ -27,7 +27,6 @@ public class BlowpipeCalculatorPanel extends PluginPanel
     private final JSpinner amount = new JSpinner(new SpinnerNumberModel(1000, 0, Integer.MAX_VALUE, 100));
     private final JComboBox<AmmoSaveRate> ammoSaveRate = new JComboBox<>(AmmoSaveRate.values());
     private final JLabel result = new JLabel();
-    private final JLabel estimateNote = new JLabel("<html><small>Based on average ammo-saving rates.</small></html>");
 
     public BlowpipeCalculatorPanel(BlowpipeCalculatorConfig config, ConfigManager configManager)
     {
@@ -60,12 +59,11 @@ public class BlowpipeCalculatorPanel extends PluginPanel
         ammoSaveRate.setSelectedItem(config.ammoSaveRate());
         ammoPanel.add(ammoSaveRate, BorderLayout.CENTER);
 
-        JPanel center = new JPanel(new GridLayout(4, 1, 0, 8));
+        JPanel center = new JPanel(new GridLayout(3, 1, 0, 8));
         center.setBackground(ColorScheme.DARK_GRAY_COLOR);
         center.add(inputPanel);
         center.add(ammoPanel);
         center.add(result);
-        center.add(estimateNote);
 
         dartsToScales.addActionListener(e -> updateMode());
         scalesToDarts.addActionListener(e -> updateMode());
