5f645299d2477176db688c19d98f95990f3515dd
ebpicgamer-spec <ebpicgamer@gmail.com>
2026-09-20 09:18:40 -0700

    Add src/main/java/com/blowpipecalculator/BlowpipeCalculator.java

---
+++ src/main/java/com/blowpipecalculator/BlowpipeCalculator.java
@@ -0,0 +1,42 @@
+package com.blowpipecalculator;
+
+public final class BlowpipeCalculator
+{
+    private static final double SCALES_PER_ATTACK = 2.0 / 3.0;
+
+    private BlowpipeCalculator()
+    {
+    }
+
+    public static long scalesForDarts(long darts, AmmoSaveRate ammoSaveRate)
+    {
+        if (darts < 0)
+        {
+            throw new IllegalArgumentException("Darts cannot be negative");
+        }
+
+        if (darts == 0)
+        {
+            return 0;
+        }
+
+        double attacks = darts / ammoSaveRate.getConsumptionRate();
+        return (long) Math.ceil(attacks * SCALES_PER_ATTACK);
+    }
+
+    public static long dartsForScales(long scales, AmmoSaveRate ammoSaveRate)
+    {
+        if (scales < 0)
+        {
+            throw new IllegalArgumentException("Scales cannot be negative");
+        }
+
+        if (scales == 0)
+        {
+            return 0;
+        }
+
+        double attacks = scales / SCALES_PER_ATTACK;
+        return (long) Math.floor(attacks * ammoSaveRate.getConsumptionRate());
+    }
+}
